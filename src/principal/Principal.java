45ed4fb870f2e4a52f69fa42cec6d90d66d864e6
misaelorelio <misaelorelio@hotmail.com>
2023-06-14 19:33:06 -0300

    Primeiro comite

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import javax.swing.*;
+
+public class Principal  extends JFrame {
+    public Principal(){
+        super.setVisible(true);
+        this.setSize(500, 500);
+        this.setTitle("Meu Jogo");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
