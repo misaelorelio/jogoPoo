3a1a2c898c92674b86509faef75268ba71a8e961
Misael <77083814+misaelorelio@users.noreply.github.com>
2023-06-21 20:06:35 -0300

    Desenvolvimento aula 07

--- src/modelo/Personagem.java
+++ src/modelo/Personagem.java
@@ -2,6 +2,8 @@ package modelo;
 
 import java.awt.*;
 
+import javax.swing.ImageIcon;
+
 public class Personagem {
     private int posicaoEmX;
     private int posicaoEmY;
@@ -10,4 +12,74 @@ public class Personagem {
     private Image imagem;
     private int larguraImagem;
     private int alturaImagem;
+
+    public Personagem() {
+        this.posicaoEmX = 100;
+        this.posicaoEmY = 100;
+    }
+
+    public void carregar() {
+        ImageIcon carregando = new ImageIcon("recursos\\nave.png");
+        this.imagem = carregando.getImage();
+        this.alturaImagem = this.imagem.getWidth(null);
+        this.larguraImagem = this.imagem.getHeight(null);
+    }
+
+
+    public int getPosicaoEmX() {
+        return this.posicaoEmX;
+    }
+
+    public void setPosicaoEmX(int posicaoEmX) {
+        this.posicaoEmX = posicaoEmX;
+    }
+
+    public int getPosicaoEmY() {
+        return this.posicaoEmY;
+    }
+
+    public void setPosicaoEmY(int posicaoEmY) {
+        this.posicaoEmY = posicaoEmY;
+    }
+
+    public int getDeslocamentoEmX() {
+        return this.deslocamentoEmX;
+    }
+
+    public void setDeslocamentoEmX(int deslocamentoEmX) {
+        this.deslocamentoEmX = deslocamentoEmX;
+    }
+
+    public int getDeslocamentoEmY() {
+        return this.deslocamentoEmY;
+    }
+
+    public void setDeslocamentoEmY(int deslocamentoEmY) {
+        this.deslocamentoEmY = deslocamentoEmY;
+    }
+
+    public Image getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(Image imagem) {
+        this.imagem = imagem;
+    }
+
+    public int getLarguraImagem() {
+        return this.larguraImagem;
+    }
+
+    public void setLarguraImagem(int larguraImagem) {
+        this.larguraImagem = larguraImagem;
+    }
+
+    public int getAlturaImagem() {
+        return this.alturaImagem;
+    }
+
+    public void setAlturaImagem(int alturaImagem) {
+        this.alturaImagem = alturaImagem;
+    }
+    
 }
