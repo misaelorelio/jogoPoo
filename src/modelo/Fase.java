488044e07a9596cfb3cc71e24bd4d18d1d0e028c
Misael <77083814+misaelorelio@users.noreply.github.com>
2023-06-21 20:06:08 -0300

    Desenvolvimento aula 07

--- src/modelo/Fase.java
+++ src/modelo/Fase.java
@@ -5,14 +5,19 @@ import java.awt.*;
 
 public class Fase extends JPanel {
     private Image fundo;
+    private Personagem personagem;
     public Fase() {
         ImageIcon carregando = new ImageIcon("recursos\\fundo.jpg");
         fundo = carregando.getImage();
+
+        personagem = new Personagem();
+        personagem.carregar();
     }
 
     public void paint (Graphics g) {
         Graphics2D graficos = (Graphics2D) g;
         graficos.drawImage(fundo, 0, 0, null);
+        graficos.drawImage(personagem.getImagem(), personagem.getPosicaoEmX(), personagem.getPosicaoEmY(), this);
         g.dispose();
     }
 
